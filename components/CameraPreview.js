67559879d855d5f13e332d42b3b6d5bf4d50f84c
Celina <celina.shen06@gmail.com>
2021-09-18 22:46:15 -0400

    Snapshotted images are now uploaded to firebase

--- components/CameraPreview.js
+++ components/CameraPreview.js
@@ -1,8 +1,34 @@
 import { StyleSheet, Text, View, TouchableOpacity, ImageBackground } from 'react-native';
 import React, {Component} from 'react';
 
+
+import firebase from '../firebase/firebaseConfig';
+
+//import firebase from '../firebase/firebaseConfig';
+// import { ref, getDownloadURL } from 'firebase/storage';
+
+// import firebase from '../firebase/firebaseConfig'
+
+import { getStorage, ref, uploadBytesResumable } from "firebase/storage"
+
+const uploadImage = async(uri) => {
+  const response = await fetch(uri);
+  const blob = await response.blob();
+  var ref = firebase.storage().ref().child("uploads/" + uri);
+  return ref.put(blob);
+}
+
 const CameraPreview = ({photo}) => {
     console.log('sdsfds', photo)
+
+    // const storage = getStorage();
+    // const snapshotRef = ref(storage, photo);
+    // const snapshotRemoteRef = ref(storage, photo);
+    // const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadImage(photo.uri)
+    
+
     return (
       <View
         style={{
